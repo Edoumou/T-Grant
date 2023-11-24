22ea58edd98b33ffa3d060ae377c3c712c2abff9
Edoumou <devforsay@gmail.com>
2023-11-24 06:20:38 +0100

    manager can now deploy the bond contract for an APPROVED deam

--- client/src/components/IssueDealForm.js
+++ client/src/components/IssueDealForm.js
@@ -1,14 +1,58 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Button, Modal, ModalActions, ModalContent, Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow } from "semantic-ui-react";
+import BankJSON from "../contracts/artifacts/contracts/Topos/Bank/ToposBank.sol/ToposBank.json";
+import BondFactoryJSON from "../contracts/artifacts/contracts/Topos/Factory/BondFactory.sol/BondFactory.json";
+import Formate from "../utils/Formate";
+import Addresses from "../addresses/addr.json";
+import { web3Connection } from "../utils/web3Connection";
+import { getContract } from "../utils/getContract";
 import "../users.css";
 import "../manager.css";
-import { Table, TableBody, TableCell, TableHeader, TableHeaderCell, TableRow } from "semantic-ui-react";
-import Formate from "../utils/Formate";
+import { setLoading } from "../store";
 
 function IssueDealForm() {
+    const [loader, setLoader] = useState(true);
+    const [showBondForm, setShowBondForm] = useState(true);
+    const [open, setOpen] = useState(false);
+    const [explorerLink, setExplorerLink] = useState('');
+    const [loadingMessage, setLoadingMessage] = useState('Transaction in Process');
+
     const bonds = useSelector(state => {
         return state.bond;
     });
+
+    const dispatch = useDispatch();
+
+    const deploy = async () => {
+        let { web3, account } = await web3Connection();
+        let bankContract = await getContract(web3, BankJSON, Addresses.ToposBankContract);
+        let bondFactory = await getContract(web3, BondFactoryJSON, Addresses.BondFactoryContract);
+
+        await bondFactory.methods.DeployBondContract(
+            bonds.dealToIssue.dealID,
+            bonds.dealToIssue.issuerAddress,
+            Addresses.ToposBankContract,
+            bonds.countryForDealToIssue
+        ).send({ from: account })
+            .on('transactionHash', hash => {
+                setLoadingMessage('Deploying the Bond Contract: ERC-7092! ⌛️');
+                setExplorerLink(`https://topos.blockscout.testnet-1.topos.technology/tx/${hash}`);
+                dispatch(setLoading(true));
+            })
+            .on('receipt', receipt => {
+                setLoadingMessage('Bond Contract Deployed! ✅');
+                setLoader(false);
+                dispatch(setLoading(false));
+            });
+
+        setShowBondForm(false);
+    }
+
+    const goToExplorer = () => {
+        const newWindow = window.open(explorerLink, '_blank', 'noopener,noreferrer');
+        if (newWindow) newWindow.opener = null;
+    }
 
     return (
         <>
@@ -36,6 +80,49 @@ function IssueDealForm() {
                     </TableBody>
                 </Table>
             </div>
+            {
+                showBondForm &&
+                <div className="deploy-bond-contract">
+                    <div className="deploy-bond-head">
+                        Deploy the Bond Contract for {bonds.dealToIssue.dealID}
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <div className="deal-button">
+                        <Modal
+                            size="tiny"
+                            open={open}
+                            trigger={
+                                <Button type='submit' color="vk" fluid size='large' onClick={deploy}>
+                                    Deploy
+                                </Button>
+                            }
+                            onClose={() => setOpen(false)}
+                            onOpen={() => setOpen(true)}
+                        >
+                            <ModalContent>
+                                <div style={{ textAlign: 'center' }}>
+                                    <h3>{loadingMessage}</h3>
+                                    {
+                                        loader ?
+                                            <Button inverted basic loading size="massive">Loading</Button>
+                                        :
+                                            <p style={{ color: 'green' }}><strong>transaction processed successfully</strong></p>
+                                    }
+                                </div>
+                            </ModalContent>
+                            <ModalActions>
+                            <Button basic floated="left" onClick={goToExplorer}>
+                                <strong>Check on Topos Explorer</strong>
+                            </Button>
+                            <Button color='black' onClick={() => setOpen(false)}>
+                                Go to Dashboard
+                            </Button>
+                            </ModalActions>
+                        </Modal>
+                    </div>
+                </div>
+            }
         </>
     );
 }
