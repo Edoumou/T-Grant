f299aed18b8e6e6a755de12ac40d9f91d98ccffc
Edoumou <devforsay@gmail.com>
2023-12-15 00:52:56 +0100

    Testing Add payment tokens addresses in Token Call Contract deployment ✅

--- client/test/Bonds.js
+++ client/test/Bonds.js
@@ -25,8 +25,14 @@ describe("Tokenized Bonds", async () => {
     let exchange;
     let exchangeStorage;
 
+    let deployer;
+    let issuer1;
+    let issuer2;
+    let investor1;
+    let investor2;
+
     beforeEach(async () => {
-        const [deployer, ...users] = await hre.ethers.getSigners();
+        [deployer, issuer1, issuer2, investor1, investor2, ...users] = await hre.ethers.getSigners();
 
         registry = await hre.ethers.deployContract("IdentityRegistry");
         authentication = await hre.ethers.deployContract("Authentication", [registry.target]);
@@ -103,5 +109,72 @@ describe("Tokenized Bonds", async () => {
         expect(couponPayment.target).not.to.equal('');
         expect(exchange.target).not.to.equal('');
         expect(exchangeStorage.target).not.to.equal('');
+    });
+
+    it("Set Funds contract addresse in Bank", async () => {
+        await bank.setIssuerFundContract(funds.target);
+
+        let fundsContract = await bank.issuersFundContract();
+
+        expect(fundsContract).to.equal(funds.target);
+    });
+    
+    it("Set Auth contract addresse in Identity Registry", async () => {
+        await registry.setAuthenticationContract(authentication.target);
+
+        let authContract = await registry.authenticationContract();
+
+        expect(authContract).to.equal(authentication.target);
+    });
+
+    it("Allows the Exchange Bonds Storage to manage bonds", async () => {
+        let isVerifiedBefore = await registry.isVerified(exchangeStorage.target);
+
+        await registry.registerContract(exchangeStorage.target);
+
+        let isVerifiedAfter = await registry.isVerified(exchangeStorage.target);
+
+        expect(isVerifiedBefore).to.be.false;
+        expect(isVerifiedAfter).to.be.true;
+    });
+
+    it("Sets contracts addresses in the Roles contract", async () => {
+        await roles.setToposContracts(bank.target, issuer, investor);
+
+        let banContract = await roles.toposBankContract();
+        let issuerContract = await roles.issuerContract();
+        let investorContract = await roles.investorContract();
+
+        expect(banContract).to.equal(bank.target);
+        expect(issuerContract).to.equal(issuer.target);
+        expect(investorContract).to.equal(investor.target);
+    });
+
+    it("Sets the Exchange Bonds storage contract address in the Exchange contract", async () => {
+        await exchange.setExchangeBondsStorage(exchangeStorage.target);
+
+        let exchangeStorageContract = await exchange.exchangeBondsStorage();
+
+        expect(exchangeStorageContract).to.equal(exchangeStorage.target);
+    });
+
+    it("Adds payment tokens addresses in Token Call contract", async () => {
+        await tokenCall.addTokenAddress(usdc.target);
+        await tokenCall.addTokenAddress(usdt.target);
+        await tokenCall.addTokenAddress(eurc.target);
+        await tokenCall.addTokenAddress(eurt.target);
+        await tokenCall.addTokenAddress(cnyc.target);
+        await tokenCall.addTokenAddress(cnyt.target);
+        await tokenCall.addTokenAddress(dai.target);
+
+        let tokensAddress = await tokenCall.getTokenAddresses();
+
+        expect(tokensAddress[0]).to.equal(usdc.target);
+        expect(tokensAddress[1]).to.equal(usdt.target);
+        expect(tokensAddress[2]).to.equal(eurc.target);
+        expect(tokensAddress[3]).to.equal(eurt.target);
+        expect(tokensAddress[4]).to.equal(cnyc.target);
+        expect(tokensAddress[5]).to.equal(cnyt.target);
+        expect(tokensAddress[6]).to.equal(dai.target);
     });
 });
