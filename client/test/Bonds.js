f96f49d987187f8c11b2799a68c8f870b02a3672
Edoumou <devforsay@gmail.com>
2023-12-17 02:21:45 +0100

    more tests

--- client/test/Bonds.js
+++ client/test/Bonds.js
@@ -32,8 +32,17 @@ describe("Tokenized Bonds", async () => {
     let investor1;
     let investor2;
 
+    let StakeHolderStatus = {
+        UNDEFINED: '0',
+        SUBMITTED: '1',
+        APPROVED: '2',
+        REJECTED: '3'
+    }
+
+    let maturityDate = Date.now() + 120;
+
     beforeEach(async () => {
-        [deployer, issuer1, issuer2, investor1, investor2, ...users] = await hre.ethers.getSigners();
+        [deployer, amazon, ggvcapital, user0, ...users] = await hre.ethers.getSigners();
 
         registry = await hre.ethers.deployContract("IdentityRegistry");
         authentication = await hre.ethers.deployContract("Authentication", [registry.target]);
@@ -91,6 +100,7 @@ describe("Tokenized Bonds", async () => {
         await registry.setAuthenticationContract(authentication.target);
         await registry.registerContract(exchangeStorage.target);
         await roles.setToposContracts(bank.target, issuer, investor);
+        await exchange.setExchangeBondsStorage(exchangeStorage.target);
 
         await tokenCall.addTokenAddress(usdc.target);
         await tokenCall.addTokenAddress(usdt.target);
@@ -99,6 +109,77 @@ describe("Tokenized Bonds", async () => {
         await tokenCall.addTokenAddress(cnyc.target);
         await tokenCall.addTokenAddress(cnyt.target);
         await tokenCall.addTokenAddress(dai.target);
+
+        // Register the Bond Manager
+        let authHash = await AuthenticationHash('123456', deployer);
+        let identyHash = await RegistrationHash(deployer, 'Manager', '0102030405', 'manager@topos.com');
+
+        await registry.connect(deployer).register(identyHash, authHash);
+        
+        // Register an account that will be used as issuer
+        authHash = await AuthenticationHash('123456', amazon);
+        identyHash = await RegistrationHash(amazon, 'Amazon', '0102030405', 'info@amazon.com');
+
+        await registry.connect(amazon).register(identyHash, authHash);
+        
+        // Register an account that will be used as investor
+        authHash = await AuthenticationHash('123456', ggvcapital);
+        identyHash = await RegistrationHash(ggvcapital, 'GGV Capital', '0102030405', 'info@ggvc.com');
+
+        await registry.connect(ggvcapital).register(identyHash, authHash);
+
+        // Issuer-Investor submit a request to become issuer-investor
+        let issuerData = {
+            documentURI: "https://www.amazon.com/doc/doc.pdf",
+            name: "Amzon",
+            country: "US",
+            issuerType: "CORP",
+            creditRating: "AA-",
+            carbonCredit: "1200",
+            walletAddress: amazon.address,
+            status: StakeHolderStatus.UNDEFINED,
+            index: "0",
+            logoURI: "https://www.amazon.com/doc/logo.png"
+        };
+
+        let investorData = {
+            name: "GGV Capital",
+            country: "US",
+            investorType: "VC",
+            walletAddress: ggvcapital.address,
+            status: StakeHolderStatus.UNDEFINED,
+            index: "0"
+        }
+
+        await issuer.connect(amazon).requestRegistrationIssuer(issuerData);
+        await investor.connect(ggvcapital).requestRegistrationInvestor(investorData);
+
+        // Manager approves the above issuer and investor
+        await issuer.connect(deployer).approveIssuer(amazon.address);
+        await investor.connect(deployer).approveInvestor(ggvcapital.address);
+
+        // Issuer Submits a Deal
+        let deals = await bank.connect(amazon).getListOfDeals();
+
+        let deal = {
+            dealID: "DEAL-001",
+            prospectusURI: "https://www.amazon.com/doc/deal.pdf",
+            issuerAddress: amazon.address,
+            debtAmount: "1000000",
+            denomination: "100",
+            couponRate: "250",
+            couponFrequency: "2",
+            maturityDate: maturityDate,
+            index: Number(deals.length) + '',
+            currency: usdc.target,
+            couponType: "1",
+            status: "0"
+        }
+
+        await bank.connect(amazon).submitDeal("DEAL-001", deal);
+
+        // manager approves the deal
+        await bank.connect(deployer).approveDeal("DEAL-001");
     });
 
     it("Checks deployment", async () => {
@@ -155,9 +236,7 @@ describe("Tokenized Bonds", async () => {
         expect(investorContract).to.equal(investor.target);
     });
 
-    it("Sets the Exchange Bonds storage contract address in the Exchange contract", async () => {
-        await exchange.setExchangeBondsStorage(exchangeStorage.target);
-
+    it("Checks the Exchange Bonds storage contract address in the Exchange contract", async () => {
         let exchangeStorageContract = await exchange.exchangeBondsStorage();
 
         expect(exchangeStorageContract).to.equal(exchangeStorage.target);
@@ -175,19 +254,84 @@ describe("Tokenized Bonds", async () => {
         expect(tokensAddress[6]).to.equal(dai.target);
     });
     
-    it("Registers Topos manager", async () => {
-        let authHash = await AuthenticationHash('123456', deployer);
-        let identyHash = await RegistrationHash(deployer, 'Manager', '0102030405', 'manager@topos.com');
+    it("Checks the bond manager is registered", async () => {
+        let isRegistered = await registry.isVerified(deployer.address);
 
-        await registry.register(identyHash, authHash);
+        expect(isRegistered).to.be.true;
     });
 
-    it("Registers issuers", async () => {
+    it("Checks Amazon is registered as an issuer", async () => {
+        let amazonData = await issuer.connect(amazon).issuers(amazon.address);
 
+        expect(amazonData.documentURI).to.equal("https://www.amazon.com/doc/doc.pdf");
+        expect(amazonData.name).to.equal("Amzon");
+        expect(amazonData.country).to.equal("US");
+        expect(amazonData.issuerType).to.equal("CORP");
+        expect(amazonData.creditRating).to.equal("AA-");
+        expect(amazonData.carbonCredit).to.equal("1200");
+        expect(amazonData.walletAddress).to.equal(amazon.address);
+        expect(amazonData.status).to.equal(StakeHolderStatus.APPROVED);
+        expect(amazonData.index).to.equal("0");
+        expect(amazonData.logoURI).to.equal("https://www.amazon.com/doc/logo.png");
     });
 
-    it("Registers investors", async () => {
+    it("Checks GGV Capital is registered as an investor", async () => {
+        let ggvcapitalData = await investor.connect(ggvcapital).investors(ggvcapital.address);
 
+        expect(ggvcapitalData.name).to.equal("GGV Capital");
+        expect(ggvcapitalData.country).to.equal("US");
+        expect(ggvcapitalData.investorType).to.equal("VC");
+        expect(ggvcapitalData.walletAddress).to.equal(ggvcapital.address);
+        expect(ggvcapitalData.status).to.equal(StakeHolderStatus.APPROVED);
+        expect(ggvcapitalData.index).to.equal("0");
+    });
+
+    it("Checks the Amazon deal has been approved", async () => {
+        let deal = await bank.connect(deployer).deals("DEAL-001");
+
+        expect(deal.dealID).to.equal("DEAL-001");
+        expect(deal.prospectusURI).to.equal("https://www.amazon.com/doc/deal.pdf");
+        expect(deal.issuerAddress).to.equal(amazon.address);
+        expect(deal.debtAmount).to.equal("1000000");
+        expect(deal.denomination).to.equal("100");
+        expect(deal.couponRate).to.equal("250");
+        expect(deal.couponFrequency).to.equal("2");
+        expect(deal.maturityDate).to.equal(maturityDate);
+        expect(deal.index).to.equal("0");
+        expect(deal.currency).to.equal(usdc.target);
+        expect(deal.couponType).to.equal("1");
+        expect(deal.status).to.equal("2");
+    });
+
+    it("Investors can register to a deal", async () => {
+        // Mint payment tokens
+        await tokenCall.connect(ggvcapital).mint(
+            ggvcapital.address,
+            "1000000000000000000000000",
+            usdc.target
+        );
+
+        await usdc.connect(ggvcapital).approve(bank.target, "1000000000000000000000000");
+        await bank.connect(ggvcapital).registerForDeal("DEAL-001", "1000000");
+
+        let invested = await bank.connect(ggvcapital).amountInvested(ggvcapital.address, "DEAL-001");
+        
+        expect(invested.hasInvested).to.be.true;
+        expect(invested.amount).to.equal("1000000");
+    });
+
+    it("Not registered users cannot invest in deal", async () => {
+        await tokenCall.connect(user0).mint(
+            ggvcapital.address,
+            "1000000000000000000000000",
+            usdc.target
+        );
+
+        await usdc.connect(user0).approve(bank.target, "1000000000000000000000000");
+
+        expect(
+            bank.connect(user0).registerForDeal("DEAL-001", "1000000")
+        ).to.rejectedWith(Error, "ACCOUNT_NOT_AAPROVED");
     });
 });
 
